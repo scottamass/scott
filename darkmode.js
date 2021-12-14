ac7ea28c750dfc6053565a9703dadeec66ef4275
scottamass <scot.atkinson@live.co.uk>
2021-12-14 15:55:59 +0000

    local sotorage support added

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,31 @@
 const drkBtn= document.querySelector('.dark-button')
 
+if(localStorage.getItem("theme") == null){
+
+
+localStorage.setItem("theme","light");}
+
+
+let localData =localStorage.getItem("theme");
+
+if(localData == "light"){
+    drkBtn.innerHTML = "D";
+    document.body.classList.remove("dark")
+}
+else if(localData == "dark"){
+    drkBtn.innerHTML="L";
+    document.body.classList.add("dark");
+
+
+}
+
 function toggleDarkMode(){
    var element =document.body;
    element.classList.toggle("dark"); 
    if(drkBtn.innerHTML==="D"){
    drkBtn.innerHTML = "L";
-}else{drkBtn.innerHTML="D";}
+   localStorage.setItem("theme","dark");
+}else{drkBtn.innerHTML="D";
+localStorage.setItem("theme","light");}
 
 }
