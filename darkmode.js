2a73b50576b7b3a41b871419a545176e178c7633
scottamass <scot.atkinson@live.co.uk>
2021-12-14 17:25:58 +0000

    fixed

--- darkmode.js
+++ darkmode.js
@@ -14,7 +14,9 @@ function toggleDarkMode(){
 }else{
     if(window?.matchMedia('(prefers-color-scheme:dark)').matches){
         document.documentElement.classList.add('light')
+        
     }else {document.documentElement.classList.add('dark')
+    
 
     }
 }
